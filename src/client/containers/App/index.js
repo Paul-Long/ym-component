396d6f7f2a403cd0eccba6c7f4d722f4aeaa588c
候宝龙 <houbaolong@niwodai.net>
2017-10-31 17:47:32 +0800

    init update

--- src/client/containers/App/index.js
+++ src/client/containers/App/index.js
@@ -1,19 +1,25 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import {Layout} from 'components';
 
 class App extends React.Component {
   render() {
+    const {children} = this.props;
     return (
       <Layout className='ym-app'>
         <Layout.Header>
           header
         </Layout.Header>
         <Layout.Content>
-          content
+          {children}
         </Layout.Content>
       </Layout>
     )
   }
 }
+
+App.propTypes = {
+  children: PropTypes.any
+};
 
 export default App;
