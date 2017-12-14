3bc50838a5c5c24d4c27348a05d791c0fdffab4b
候宝龙 <houbaolong@niwodai.net>
2017-12-14 11:56:12 +0800

    update mobile

--- src/mobile/container/home/index.js
+++ src/mobile/container/home/index.js
@@ -6,10 +6,10 @@ class Home extends React.Component {
     return (
       <div>
         <Carousel infinite
-                  dots={false}
-                  dragging={false}
+                  dots
+                  dragging
                   swiping={false}
-                  autoplay
+                  autoplay={false}
         >
           <div style={{
             background: '#108ee9',
@@ -17,7 +17,7 @@ class Home extends React.Component {
             lineHeight: '200px',
             height: '200px',
             width: '100%'
-          }}>carousel 1
+          }}><img src="http://houym-1254119810.picsh.myqcloud.com/WechatIMG100.jpeg?imageView2/0/w/200" alt="" />
           </div>
           <div style={{
             background: '#108ee9',
@@ -25,7 +25,7 @@ class Home extends React.Component {
             lineHeight: '200px',
             height: '200px',
             width: '100%'
-          }}>carousel 2
+          }}><img src="http://houym-1254119810.picsh.myqcloud.com/WechatIMG98.jpeg?imageView2/0/w/200" alt="" />
           </div>
           <div style={{
             background: '#108ee9',
@@ -33,7 +33,7 @@ class Home extends React.Component {
             lineHeight: '200px',
             height: '200px',
             width: '100%'
-          }}>carousel 3
+          }}><img src="http://houym-1254119810.picsh.myqcloud.com/WechatIMG99.jpeg?imageView2/0/w/200" alt="" />
           </div>
         </Carousel>
       </div>
