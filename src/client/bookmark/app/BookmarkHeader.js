397f6543549ed7f5027d92d390b07a188afe70b4
候宝龙 <houbaolong@niwodai.net>
2017-12-15 17:07:44 +0800

    update

--- src/client/bookmark/app/BookmarkHeader.js
+++ src/client/bookmark/app/BookmarkHeader.js
@@ -3,14 +3,22 @@ import Logo from 'app@components/logo';
 import {Layout, Menu} from 'antd';
 import {Link} from 'react-router-dom';
 
-export default ((props) => {
-  return (
-    <Layout.Header className='flex-row'>
-      <Logo color='#f56a00' />
-      <Menu mode='horizontal'>
-        <Menu.Item key='/bookmark'><Link to='/bookmark'>书签</Link></Menu.Item>
-        <Menu.Item key='/bookmark/group'><Link to='/bookmark/group'>书签组设置</Link></Menu.Item>
-      </Menu>
-    </Layout.Header>
-  );
-});
+class BookmarkHeader extends React.Component {
+  state = {
+    key: '/bookmark'
+  };
+
+  render() {
+    return (
+      <Layout.Header className='flex-row'>
+        <Logo color='#f56a00' />
+        <Menu mode='horizontal' onClick={({key}) => this.setState({key})} selectedKeys={[this.state.key]}>
+          <Menu.Item key='/bookmark'><Link to='/bookmark'>书签</Link></Menu.Item>
+          <Menu.Item key='/bookmark/group'><Link to='/bookmark/group'>书签组设置</Link></Menu.Item>
+        </Menu>
+      </Layout.Header>
+    )
+  }
+}
+
+export default BookmarkHeader;
